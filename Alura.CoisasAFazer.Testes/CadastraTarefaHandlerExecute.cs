11d0c5092fbf0c8241d6f71b37b292334d792ed9
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-10-15 11:50:40 -0300

    Iniciando o primeiro teste

---
+++ Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -0,0 +1,30 @@
+using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Core.Models;
+using Alura.CoisasAFazer.Services.Handlers;
+using System;
+using Xunit;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    public class CadastraTarefaHandlerExecute
+    {
+        [Fact]
+        public void DadaTarefaComInfoValidasDeveIncluirNoDB()
+        {
+            // arrange
+            var comando = new CadastraTarefa(
+                "Executar Xunit",
+                new Categoria("Estudo"),
+                new DateTime(2019, 12, 31)
+            );
+
+            var handler = new CadastraTarefaHandler();
+
+            // act
+            handler.Execute(comando); // SUT >> CadastraTarefaHandlerExecute
+
+            // assert
+
+        }
+    }
+}
