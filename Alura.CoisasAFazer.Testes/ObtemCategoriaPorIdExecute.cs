c418e3115beb61bf9c93990230ee92dbae8a875d
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-10-18 10:50:45 -0300

    Criando o teste "QuandoIdForExistenteDeveChamarObtemCategoriaPorIdUmaUnicaVez"

---
+++ Alura.CoisasAFazer.Testes/ObtemCategoriaPorIdExecute.cs
@@ -0,0 +1,31 @@
+﻿using Alura.CoisasAFazer.Core.Commands;
+using Alura.CoisasAFazer.Infrastructure;
+using Alura.CoisasAFazer.Services.Handlers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    public class ObtemCategoriaPorIdExecute
+    {
+        [Fact]
+        public void QuandoIdForExistenteDeveChamarObtemCategoriaPorIdUmaUnicaVez()
+        {
+            // arrange
+            var idCategoria = 20;
+            var comando = new ObtemCategoriaPorId(idCategoria);
+            var mock = new Mock<IRepositorioTarefas>();
+            var repo = mock.Object;
+            var handler = new ObtemCategoriaPorIdHandler(repo);
+
+            // act
+            handler.Execute(comando);
+
+            // assert
+            mock.Verify(x => x.ObtemCategoriaPorId(idCategoria), Times.Once());
+        }
+    }
+}
