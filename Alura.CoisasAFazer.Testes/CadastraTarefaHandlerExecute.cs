7e979930cc3a9e2dfb139cb9a9fc8284ac8b48f1
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-10-18 18:11:16 -0300

    Criando um teste para testar o log

--- Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
+++ Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -7,6 +7,7 @@ using System;
 using System.Linq;
 using Xunit;
 using Moq;
+using Microsoft.Extensions.Logging;
 
 namespace Alura.CoisasAFazer.Testes
 {
@@ -22,13 +23,15 @@ namespace Alura.CoisasAFazer.Testes
                 new DateTime(2019, 12, 31)
             );
 
+            var mock = new Mock<ILogger<CadastraTarefaHandler>>();
+
             var options = new DbContextOptionsBuilder<DbTarefasContext>()
                 .UseInMemoryDatabase("DbTarefasContext")
                 .Options;
 
             var contexto = new DbTarefasContext(options);
             var repo = new RepositorioFake();
-            var handler = new CadastraTarefaHandler(repo);
+            var handler = new CadastraTarefaHandler(repo, mock.Object);
 
             // act
             handler.Execute(comando); // SUT >> CadastraTarefaHandlerExecute
@@ -51,19 +54,55 @@ namespace Alura.CoisasAFazer.Testes
                 new DateTime(2019, 12, 31)
             );
 
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
             var mock = new Mock<IRepositorioTarefas>();
 
             mock.Setup(x => x.IncluirTarefas(It.IsAny<Tarefa[]>()))
                 .Throws(new Exception("Houve um erro na inclusão de tarefas"));
 
             var repo = mock.Object;
-            var handler = new CadastraTarefaHandler(repo);
+            var handler = new CadastraTarefaHandler(repo, mockLogger.Object);
 
             // act
             CommandResult resultado = handler.Execute(comando);
 
             // assert
             Assert.False(resultado.IsSuccess);
+        }
+
+        [Fact]
+        public void QuandoExceptionForLancadaDeveLogarAMensagemDaExcecao()
+        {
+            // arrange
+            var mensagemDeErroEsperada = "Houve um erro na inclusão de tarefas";
+            var excecaoEsperada = new Exception(mensagemDeErroEsperada);
+
+            var comando = new CadastraTarefa(
+                "Executar Xunit",
+                new Categoria("Estudo"),
+                new DateTime(2019, 12, 31)
+            );
+
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+            var mock = new Mock<IRepositorioTarefas>();
+
+            mock.Setup(x => x.IncluirTarefas(It.IsAny<Tarefa[]>()))
+                .Throws(excecaoEsperada);
+
+            var repo = mock.Object;
+            var handler = new CadastraTarefaHandler(repo, mockLogger.Object);
+
+            // act
+            CommandResult resultado = handler.Execute(comando);
+
+            // assert
+            mockLogger.Verify(x => x.Log(
+                LogLevel.Error, //nível de log => LogError
+                It.IsAny<EventId>(), //identificador do evento
+                It.IsAny<object>(), //objeto que será logado
+                excecaoEsperada, //exceção que será logada 
+                It.IsAny<Func<object, Exception, string>>() // função que converte objeto+exceção >> string
+            ), Times.Once());
         }
     }
 }
--- src/Alura.CoisasAFazer.Services/Handlers/CadastraTarefaHandler.cs
+++ src/Alura.CoisasAFazer.Services/Handlers/CadastraTarefaHandler.cs
@@ -11,14 +11,12 @@ namespace Alura.CoisasAFazer.Services.Handlers
         IRepositorioTarefas _repo;
         ILogger<CadastraTarefaHandler> _logger;
 
-        public CadastraTarefaHandler()
-        {
-        }
-
-        public CadastraTarefaHandler(IRepositorioTarefas repositorio)
+        public CadastraTarefaHandler(
+            IRepositorioTarefas repositorio,
+            ILogger<CadastraTarefaHandler> logger)
         {
             _repo = repositorio;
-            _logger = new LoggerFactory().CreateLogger<CadastraTarefaHandler>();
+            _logger = logger;
         }
 
         public CommandResult Execute(CadastraTarefa comando)
@@ -40,8 +38,9 @@ namespace Alura.CoisasAFazer.Services.Handlers
 
                 return new CommandResult(true);
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                _logger.LogError(e, e.Message);
                 return new CommandResult(false);
             }
         }
