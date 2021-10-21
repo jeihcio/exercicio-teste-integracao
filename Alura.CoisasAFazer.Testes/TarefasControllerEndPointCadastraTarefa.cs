5e97d10a3c8c49e45585b45417cbf74034e7d6af
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-10-21 11:50:00 -0300

    Testando a aplicação web

---
+++ Alura.CoisasAFazer.Testes/TarefasControllerEndPointCadastraTarefa.cs
@@ -0,0 +1,51 @@
+﻿using Alura.CoisasAFazer.Core.Models;
+using Alura.CoisasAFazer.Infrastructure;
+using Alura.CoisasAFazer.Services.Handlers;
+using Alura.CoisasAFazer.WebApp.Controllers;
+using Alura.CoisasAFazer.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    public class TarefasControllerEndPointCadastraTarefa
+    {
+        [Fact]
+        public void DadaTarefaComInformacoesValidasDeveRetornar200()
+        {
+            // arrange
+            var mockLogger = new Mock<ILogger<CadastraTarefaHandler>>();
+
+            var options = new DbContextOptionsBuilder<DbTarefasContext>()
+                .UseInMemoryDatabase("DbTarefasContext")
+                .Options;
+
+            var contexto = new DbTarefasContext(options);
+
+            contexto.Categorias.Add(new Categoria(20, "Estudo"));
+            contexto.SaveChanges();
+
+            var repo = new RepositorioTarefa(contexto);
+
+            var controlador = new TarefasController(repo, mockLogger.Object);
+            var model = new CadastraTarefaVM()
+            {
+                IdCategoria = 20,
+                Titulo = "Estudar xunit",
+                Prazo = new DateTime(2019, 12, 31)
+            };
+
+            // act
+            var retorno = controlador.EndpointCadastraTarefa(model);
+
+            // assert
+            Assert.IsType<OkResult>(retorno);
+        }
+    }
+}
